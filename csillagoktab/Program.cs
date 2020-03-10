a1e38a2d6caada6fe3f474db7826cf748f2028c8
fekete.alexandra <fekete.alexandra@C41-12>
2020-03-10 13:13:54 +0100

    csillagok

--- csillagoktab/Program.cs
+++ csillagoktab/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace csillagoktab
 {
@@ -10,6 +11,23 @@ namespace csillagoktab
     {
         static void Main(string[] args)
         {
+            List<string> sorok = new List<string>();
+            StreamReader olvasó = new StreamReader("CsillagokTab.txt"); //egy egesz szöveget visszakomentelni ctrl k ctrl u
+            while (!olvasó.EndOfStream)
+            {
+                sorok.Add(olvasó.ReadLine());
+                
+            }
+            olvasó.Close();
+            //string[] sorok2= File.ReadAllLines("Csillagoktab.txt");
+            /*foreach (string sor in sorok)
+            {
+                Console.WriteLine(sorok[4]);
+            }*/
+            string aktsor = sorok[4];
+            string[] soradatok = aktsor.Split('\t');
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
